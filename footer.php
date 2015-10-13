e91c20ef85a8370105bc5f705bd55f0cedcb6ad3
JeppeSigaard <jeppe@smatmonkey.dk>
2015-10-13 10:56:36 +0200

    footer of the year

--- footer.php
+++ footer.php
@@ -8,42 +8,49 @@ $options = get_option( 'footer_options' );
 
 ?>
 <footer id="footer">
-    <div>
-       <?php if (isset($options['field_0'])) :?>
-           <?php echo $options['field_0'] ?> · 
-       <?php endif; ?>
-       <?php if (isset($options['field_1'])) :?>
-           <?php echo $options['field_1'] ?> · 
-       <?php endif; ?>
-       <?php if (isset($options['field_2'])) :?>
-           <?php echo $options['field_2'] ?> · 
-       <?php endif; ?>
-       <?php if (isset($options['field_3'])) :?>
-           <?php echo $options['field_3'] ?> · 
-       <?php endif; ?>
-       <?php if (isset($options['field_4'])) :?>
-           <a href="mailto:<?php echo $options['field_4'] ?>"><?php echo $options['field_4'] ?></a>
-       <?php endif; ?>
-       
-       <br/>
-       
-       <?php if (isset($options['field_5'])) :?>
-           EAN: <?php echo $options['field_5'] ?> · 
-       <?php endif; ?>
-       <?php if (isset($options['field_6'])) :?>
-           CVR: <?php echo $options['field_6'] ?> · 
-       <?php endif; ?>
-       <?php if (isset($options['field_7'])) :?>
-           Skolenummer: <?php echo $options['field_7'] ?> · 
-       <?php endif; ?>
-       <?php if (isset($options['field_8'])) :?>
-           Eksamensnummer: <?php echo $options['field_8'] ?> 
-       <?php endif; ?>
+    <div class="footer-inner">
+        <a class="footer-logo" href="<?php echo esc_url(get_bloginfo('url')); ?>"><img src="<?php echo get_template_directory_uri() ?>/statics/fg-logo.png"></a>
+        <div class="footer-column">
+            <div>
+                <p><strong><?php echo (isset($options['field_0'])) ? esc_attr($options['field_0']) : ''; ?></strong></p>
+                <p><?php echo (isset($options['field_1'])) ? esc_attr($options['field_1']) : ''; ?></p> 
+                <p><?php echo (isset($options['field_2'])) ? esc_attr($options['field_2']) : ''; ?></p>
+            </div>
+            <div>
+                <p><strong>EAN-nummer </strong> <?php echo (isset($options['field_5'])) ? esc_attr($options['field_5']) : ''; ?></p>
+                <p><strong>CVR-nummer </strong> <?php echo (isset($options['field_6'])) ? esc_attr($options['field_6']) : ''; ?></p>
+            </div>
+            <div>
+                <p><strong>Skolenummer </strong> <?php echo (isset($options['field_7'])) ? esc_attr($options['field_7']) : ''; ?></p>
+                <p><strong>Eksamensnummer </strong> <?php echo (isset($options['field_8'])) ? esc_attr($options['field_8']) : ''; ?></p>
+            </div>
+        </div>
+        <div class="footer-column">
+            <div>
+                <p><strong>Telefon</strong> <?php echo (isset($options['field_3'])) ? esc_attr($options['field_3']) : ''; ?></p>
+                <p><strong>Email</strong> <?php echo (isset($options['field_4'])) ? esc_attr($options['field_4']) : ''; ?></p>
+            </div>
+            <div>
+                <p><strong>Kontorets åbninstid</strong></p>
+                <p>7.45 - 14.30</p>
+            </div>
+        </div>
+        <div class="footer-column footer-menu">
+            <div>
+                <p><strong>LINKS</strong></p>
+                <?php wp_nav_menu(
+                    array(
+                        'theme_location' => 'footer-menu',
+                        'container'       => false,
+                        'menu_class'      => 'footer-menu',
+                        'menu_id'         => false,
+                        'fallback_cb'     => false,
+                        'items_wrap'      => '<ul class="%2$s">%3$s</ul>',
+                    )
+                ); ?>    
+            </div>
+        </div>
     </div>
-    
-    <a href="<?php echo get_bloginfo('url'); ?>">
-        <img src="<?php header_image(); ?>" alt="<?php echo get_bloginfo(); ?>" />
-    </a>
 </footer>
 <?php wp_footer(); ?>
 </body>
--- functions/menus.php
+++ functions/menus.php
@@ -5,6 +5,7 @@ register_nav_menus(
         'main-menu' => __( 'Hovedmenu', 'smartmonkey' ),
         'front-menu' => __( 'Forsidens sidemenu', 'smartmonkey' ),
         'metro-menu' => __( 'Skrivemetro', 'smartmonkey' ),
+        'footer-menu' => __( 'Footermenu', 'smartmonkey' ),
 
     )
 );
